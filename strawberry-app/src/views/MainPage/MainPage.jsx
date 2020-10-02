b72acca5e63129148928c3bc50eb3dbbcb090a5b
Chloe Ann Basson <21555745@sun.ac.za>
2020-10-02 14:42:25 +0200

    [ADD] added routes

--- strawberry-app/src/views/MainPage/MainPage.jsx
+++ strawberry-app/src/views/MainPage/MainPage.jsx
@@ -1,1 +1,15 @@
 //TODO main page here
+import React, { Component } from 'react'
+
+class MainPage extends Component {
+  render () {
+    console.log("TEST")
+    return (
+      <div className="main">
+        <h3>Hello</h3>
+      </div>
+    )
+  }
+}
+
+export default MainPage;
