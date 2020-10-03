50b78280ec869f4faf620e751cd34ef7f8a9e1a8
ginalamp <gina.n.lamprecht@gmail.com>
2020-10-03 10:57:39 +0200

    Functional but ugly chat

--- strawberry-app/src/views/Chat/Chat.jsx
+++ strawberry-app/src/views/Chat/Chat.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import MessageList from './MessageList'
+import SendMessageForm from './SendMessageForm'
 
 class Chat extends Component {
-  render () {
+  constructor() {
+    super()
+    this.state = {
+        messages: []
+    }
+    this.sendMessage = this.sendMessage.bind(this)
+  }
+  
+  
+  sendMessage(text) {
+    console.log("message sent", text);
+
+    var joined = this.state.messages.concat({text: text, index: this.state.messages.length});
+    this.setState({ messages: joined })
+  }
+
+
+  render() {
     return (
       <div className="chat_app">
+        {/* <ChatBubble messages = {this.state.messages} /> */}
 
+        <MessageList
+          messages={this.state.messages} />
+        <SendMessageForm
+          sendMessage={this.sendMessage} />
       </div>
-    )
+    );
   }
 }
 
---
+++ strawberry-app/src/views/Chat/MessageList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Message from './Message'
+
+class MessageList extends React.Component {
+    componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this);
+        this.shouldScrollBottom = node.scrollTop + node.clientHeight + 50 >= node.scrollHeight;
+    }
+
+    componentDidUpdate() {
+        if (this.shouldScrollBottom) {
+            const node = ReactDOM.findDOMNode(this);
+            node.scrollTop = node.scrollHeight
+        }
+    }
+        
+    render() {
+        return (
+            <div className="message-list">
+                {this.props.messages.map(message => {
+                    return <Message
+                        key={message.index}
+                        user={"Anonymous"}
+                        text={message.text}
+                    /> 
+                })}
+            </div>
+        )
+    }
+}
+
+export default MessageList
